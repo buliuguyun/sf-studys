fa1611a3599fa01d44b658cb420518763d854ae2
shifan <shifan1024@126.com>
2021-06-04 15:26:53 +0800

    初始化

--- src/entry.js
+++
@@ -1,7 +0,0 @@
-console.log('22222')
-import './css/index.css';
-import './css/lessIndex.less';
-{
-  let jspangString = 'Hello Webpack'
-  document.getElementById('test').innerHTML=jspangString; 
-}
